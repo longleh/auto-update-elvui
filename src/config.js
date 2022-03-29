4d984c6d1334f022c5f392be0d5698afc23a27aa
longleh <33480010+longleh@users.noreply.github.com>
2022-03-29 23:47:31 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  wow_folder: "C:/Users/longle_h/Documents/GitHub/auto-update-elvui/wow_test_empty/",
+  wow_folder: "",
   elvui_website: "https://www.tukui.org/",
   elvui_page: "download.php?ui=elvui",
 };
